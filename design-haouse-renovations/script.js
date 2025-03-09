6834cdcce4637b5dda599774bdbbf787e4068c05
marumaru4649 <k09160908@yahoo.co.jp>
2025-03-09 15:36:13 +0900

    Add files via upload

---
+++ design-haouse-renovations/script.js
@@ -0,0 +1,21 @@
+'use strict';
+document.getElementById('conc').onclick= function(){
+  document.getElementById('menu-btn').checked = false;
+    console.log('a');
+  }
+  document.getElementById('ser').onclick= function(){
+    document.getElementById('menu-btn').checked = false;
+      console.log('a');
+    }
+    document.getElementById('flo').onclick= function(){
+      document.getElementById('menu-btn').checked = false;
+        console.log('a');
+      }
+      document.getElementById('que').onclick= function(){
+        document.getElementById('menu-btn').checked = false;
+          console.log('a');
+        }
+        document.getElementById('cont').onclick= function(){
+          document.getElementById('menu-btn').checked = false;
+            console.log('a');
+          }
